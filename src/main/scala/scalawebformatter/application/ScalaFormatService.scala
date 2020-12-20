6649dc46f3d44faf0027b2e8c09007a80576475b
tobita_yoshiki <>
2020-12-20 18:07:04 +0900

    Add ScalaFormatService

--- src/main/scala/scalawebformatter/application/ScalaFormatService.scala
+++ src/main/scala/scalawebformatter/application/ScalaFormatService.scala
@@ -1,13 +1,15 @@
 package scalawebformatter.application
 
-import scalawebformatter.domain.model.response.ErrorMessage
+import scalawebformatter.domain.model.format.FormatConfig
+import scalawebformatter.domain.model.response.{ErrorMessage, FormattedString}
 
-class ScalaFormatService(scalaParser: ScalaParser) {
-  def format(input: String) = {
+class ScalaFormatService(scalaParser: ScalaParser, scalaFormatter: ScalaFormatter) {
+  def format(input: String, formatConfig: FormatConfig): Either[ErrorMessage, FormattedString] = {
     scalaParser.parse(input) match {
       case Left(value) => Left(ErrorMessage(value))
       case Right(value) =>
-        ???
+        val formattedStructure = scalaFormatter.format(value, formatConfig)
+        Right(scalaFormatter.format(formattedStructure, formatConfig))
     }
   }
 }
